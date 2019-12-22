09f92421aec24fe42bbfbe121df62e5060f15ba5
Maureen Josephine <Maureen Josephine>
2019-12-22 18:39:27 +0300

    add logger and scheduled annotations

--- src/main/java/hello/ScheduleTasks.java
+++ src/main/java/hello/ScheduleTasks.java
@@ -3,9 +3,19 @@ package hello;
 import org.springframework.stereotype.Component;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+import  org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduleTasks {
 
+    private static final Logger log = LoggerFactory.getLogger(ScheduleTasks.class);
+    private static final SimpleDateFormat dateformat = new SimpleDateFormat("HH:mm:ss");
+@Scheduled(fixedRate = 5000)
+    public void reportCurrentTime(){
+    log.info("The time is now {}", dateformat.format(new Date()));
 }
+}
+///The scheduled annotation defines when a particular method runs at a fixed rate
