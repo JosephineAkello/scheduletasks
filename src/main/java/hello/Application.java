a0fce86a0dca8b002735683e3d869771323d301f
Maureen Josephine <Maureen Josephine>
2019-12-22 20:01:38 +0300

    enable scheduling

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,4 +1,14 @@
 package hello;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling //ensures that background task executor is created
 public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class);
+    }
 }
